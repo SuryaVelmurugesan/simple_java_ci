addb4b7b4e2bd562a304ff79370def3e44924a70
SuryaVelmurugesan <suryavelmurugesan@gmail.com>
2025-09-14 20:56:15 +0530

    Testing pipeline

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to CI/CD Pipeline Example!Testing 123...");
+        System.out.println("Welcome to CI/CD Pipeline Example!Testing 123");
     }
 }
